351b8a5017b5549a9cec5f103fcd4660624e4d15
webabdulla <webabdullah7410@gmail.com>
2023-06-11 12:24:23 +0600

    port added

--- index.js
+++ index.js
@@ -10,6 +10,55 @@ app.use(cors());
 app.use(express.json());
 
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tbnspgu.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    const classCollection = client.db("fitcraftDb").collection("classes");
+    const instructorsCollection = client.db("fitcraftDb").collection("instructors");
+    const cartsCollection = client.db("fitcraftDb").collection("carts");
+
+    app.get('/classes',async (req,res) =>{
+        const result = await classCollection.find().toArray();
+        res.send(result);
+    })
+    app.get('/instructors',async (req,res) =>{
+        const result = await instructorsCollection.find().toArray();
+        res.send(result);
+    })
+
+    //cart collection
+    app.post('/carts',async (req,res) => {
+      const item =req.body;
+      const result = await cartsCollection.insertOne(item);
+      res.send(result);
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 app.get('/',(req,res)=>{
     res.send('Join Fitcraft Academy')
 })
